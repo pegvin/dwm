37beadb2b05574a64f61b954a2b984364e56531d
pegvin <75035219+pegvin@users.noreply.github.com>
2024-01-25 21:26:23 +0530

    Update Font & Color

--- config.h
+++ config.h
@@ -12,13 +12,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Liberation Mono:size=10" };
-static const char dmenufont[]       = "Liberation Mono:size=10";
+static const char *fonts[]          = { "Atkinson Hyperlegible:size=11" };
+static const char dmenufont[]       = "Atkinson Hyperlegible:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#2867d4";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
